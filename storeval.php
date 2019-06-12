442a914f50ff637e3ee7419f3e6119e33d11423e
Morne <morneventer.mv@gmail.com>
2019-06-12 12:21:41 +0200

    record fixes

--- storeval.php
+++ storeval.php
@@ -42,7 +42,7 @@
      $sql = ("INSERT INTO programs (program1, program2, program3, program4, program5, program6, program7, program8, DateAdded, program1_leng, program2_leng, program3_leng, program4_leng, program5_leng, program6_leng, program7_leng, program8_leng) VALUES ($program1,$program2,$program3,$program4,$program5,$program6,$program7,$program8,SYSDATE(),$program1_ex,$program2_ex,$program3_ex,$program4_ex,$program5_ex,$program6_ex,$program7_ex,$program8_ex)");
 
       if ($conn->query($sql) === TRUE) {
-          echo "ID of last inserted record is: " . mysql_insert_id($conn);
+          echo "ID of last inserted record is: " . mysqli_insert_id($conn);
         //  $_SESSION["lastID"] = mysql_insert_id();
           echo "New record created successfully";
           header('Location: /sim.html');
