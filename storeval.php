1c1be9fd22399081197df8c439c92a2e4e298a88
Morne <morneventer.mv@gmail.com>
2019-06-12 11:19:29 +0200

    fixed php maybe

--- storeval.php
+++ storeval.php
@@ -7,7 +7,7 @@
   $db = 'Project2';
 
   $dbconn = mysql_connect($dbhost, $dbuser, $dbpass);
-  mysql_select_db($db);
+  mysql_select_db($db,$dbconn);
 
   if (isset($_POST['submit']))
   {
@@ -36,11 +36,7 @@
      $program8_ex   = $_POST['executeTimeH'];
 
 
-
-
-      $query = ("INSERT INTO
-                `programs`(`program1`, `program2`, `program3`, `program4`, `program5`, `program6`, `program7`, `program8`, `DateAdded`, `program1_leng`, `program2_leng`, `program3_leng`, `program4_leng`, `program5_leng`, `program6_leng`, `program7_leng`, `program8_leng`)
-                VALUES ($program1,$program2,$program3,$program4,$program5,$program6,$program7,$program8,SYSDATE(),$program1_ex,$program2_ex,$program3_ex,$program4_ex,$program5_ex,$program6_ex,$program7_ex,$program8_ex");
+     $query = ("INSERT INTO `programs`(`program1`, `program2`, `program3`, `program4`, `program5`, `program6`, `program7`, `program8`, `DateAdded`, `program1_leng`, `program2_leng`, `program3_leng`, `program4_leng`, `program5_leng`, `program6_leng`, `program7_leng`, `program8_leng`) VALUES ($program1,$program2,$program3,$program4,$program5,$program6,$program7,$program8,SYSDATE(),$program1_ex,$program2_ex,$program3_ex,$program4_ex,$program5_ex,$program6_ex,$program7_ex,$program8_ex");
 
       if(mysql_query($query))
       {
