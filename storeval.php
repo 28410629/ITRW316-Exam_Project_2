c8829f76f1642712c88ccb1b4e30374b36a6f588
Morne <morneventer.mv@gmail.com>
2019-06-12 11:51:51 +0200

    sql test

--- storeval.php
+++ storeval.php
@@ -40,8 +40,8 @@
      $program8_ex   = $_POST['executeTimeH'];
 
 
-      $sql = ("INSERT INTO programs (program1, program2, program3, program4, program5, program6, program7, program8, DateAdded, program1_leng, program2_leng, program3_leng, program4_leng, program5_leng, program6_leng, program7_leng, program8_leng) VALUES ($program1,$program2,$program3,$program4,$program5,$program6,$program7,$program8,SYSDATE(),$program1_ex,$program2_ex,$program3_ex,$program4_ex,$program5_ex,$program6_ex,$program7_ex,$program8_ex)");
-
+    //  $sql = ("INSERT INTO programs (program1, program2, program3, program4, program5, program6, program7, program8, DateAdded, program1_leng, program2_leng, program3_leng, program4_leng, program5_leng, program6_leng, program7_leng, program8_leng) VALUES ($program1,$program2,$program3,$program4,$program5,$program6,$program7,$program8,SYSDATE(),$program1_ex,$program2_ex,$program3_ex,$program4_ex,$program5_ex,$program6_ex,$program7_ex,$program8_ex)");
+      $sql = ("INSERT INTO programs (program1) VALUES ('55')");
       if ($conn->query($sql) === TRUE) {
           echo "New record created successfully";
       }
